e3c1b99887df77a9a3cee7286307e7f5554b29b5
beatrizGJ <beatrizgarcia.jim@gmail.com>
2020-06-18 15:54:22 +0200

    init route skeleton

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+//settings
+console.log(process.env.PORT)
+app.set('port', process.env.PORT || 4000);
 
+//middelwares
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/notes', require('./routes/notes'));
 
 
 module.exports = app;
---
+++ backend/src/routes/users.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const router = Router();
+
+router.route('/')
+  .get((req, res) => res.send('Users'))
+//.post()
+
+router.route('/:id')
+//.get()
+//.put()
+//.delete()
+
+
+
+
+module.exports = router;
