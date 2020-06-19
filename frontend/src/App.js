e83046a475ec24f2ce216a4659d7c7efcd6156d6
beatrizGJ <beatrizgarcia.jim@gmail.com>
2020-06-19 10:06:04 +0200

    add frontend, created react skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from './components/Navigation'
+import NoteList from './components/NoteList'
+import CreateNote from './components/CreateNote'
+import CreateUser from './components/CreateUser'
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Route path="/" exact component={NoteList} />
+      <Route path="/edit/:id" component={CreateNote} />
+      <Route path="/create" component={CreateNote} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
+  );
+}
+
+export default App;
